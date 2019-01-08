12e61b761bf71bd606cb2be4999ca95f348be1c6
FalsinSoft <falsinsoft@gmail.com>
2019-01-08 22:39:51 +0100

    Renamed some library sources

--- QtAndroidTools/QtAndroidTools.cpp
+++ QtAndroidTools/QtAndroidTools.cpp
@@ -1,7 +1,29 @@
-
+/*
+ *	MIT License
+ *
+ *	Copyright (c) 2018 Fabio Falsini <falsinsoft@gmail.com>
+ *
+ *	Permission is hereby granted, free of charge, to any person obtaining a copy
+ *	of this software and associated documentation files (the "Software"), to deal
+ *	in the Software without restriction, including without limitation the rights
+ *	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *	copies of the Software, and to permit persons to whom the Software is
+ *	furnished to do so, subject to the following conditions:
+ *
+ *	The above copyright notice and this permission notice shall be included in all
+ *	copies or substantial portions of the Software.
+ *
+ *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *	SOFTWARE.
+ */
 #include <QQmlEngine>
-#include "QtAndroidAppPermissions.h"
-#include "QtAndroidApkExpansionFiles.h"
+#include "QAndroidAppPermissions.h"
+#include "QAndroidApkExpansionFiles.h"
 #include "QtAndroidTools.h"
 
 QtAndroidTools::QtAndroidTools()
@@ -10,6 +32,6 @@ QtAndroidTools::QtAndroidTools()
 
 void QtAndroidTools::InitializeQmlTools()
 {
-    qmlRegisterSingletonType<QtAndroidAppPermissions>("AndroidTools.AppPermissions", 1, 0, "QtAndroidAppPermissions", &QtAndroidAppPermissions::qmlInstance);
-    qmlRegisterSingletonType<QtAndroidApkExpansionFiles>("AndroidTools.ApkExpansionFiles", 1, 0, "QtAndroidApkExpansionFiles", &QtAndroidApkExpansionFiles::qmlInstance);
+    qmlRegisterSingletonType<QAndroidAppPermissions>("QtAndroidTools.AppPermissions", 1, 0, "QtAndroidAppPermissions", &QAndroidAppPermissions::qmlInstance);
+    qmlRegisterSingletonType<QAndroidApkExpansionFiles>("QtAndroidTools.ApkExpansionFiles", 1, 0, "QtAndroidApkExpansionFiles", &QAndroidApkExpansionFiles::qmlInstance);
 }
--- QtAndroidTools/QtAndroidTools.pri
+++ QtAndroidTools/QtAndroidTools.pri
@@ -4,16 +4,14 @@ QT += androidextras
 HEADERS += $$PWD/QtAndroidTools.h
 SOURCES += $$PWD/QtAndroidTools.cpp
 
-HEADERS += $$PWD/QtAndroidAppPermissions.h
-SOURCES += $$PWD/QtAndroidAppPermissions.cpp
+HEADERS += $$PWD/QAndroidAppPermissions.h
+SOURCES += $$PWD/QAndroidAppPermissions.cpp
 
-HEADERS += $$PWD/QtAndroidApkExpansionFiles.h
-SOURCES += $$PWD/QtAndroidApkExpansionFiles.cpp
-OTHER_FILES += $$PWD/src/com/falsinsoft/QtAndroidTools/ApkExpansionDownloader.java \
-               $$PWD/src/com/falsinsoft/QtAndroidTools/ApkExpansionDownloaderService.java \
-               $$PWD/src/com/falsinsoft/QtAndroidTools/ApkExpansionDownloaderAlarmReceiver.java
+HEADERS += $$PWD/QAndroidApkExpansionFiles.h
+SOURCES += $$PWD/QAndroidApkExpansionFiles.cpp
+OTHER_FILES += $$PWD/src/com/falsinsoft/qtandroidtools/ApkExpansionDownloader.java
 
-copy_src.commands = $(CHK_DIR_EXISTS) $$shell_path($$ANDROID_PACKAGE_SOURCE_DIR/src/com/falsinsoft/QtAndroidTools) $(COPY_DIR) $$shell_path($$PWD/src) $$shell_path($$ANDROID_PACKAGE_SOURCE_DIR/src)
+copy_src.commands = $(CHK_DIR_EXISTS) $$shell_path($$ANDROID_PACKAGE_SOURCE_DIR/src/com/falsinsoft/qtandroidtools) $(COPY_DIR) $$shell_path($$PWD/src) $$shell_path($$ANDROID_PACKAGE_SOURCE_DIR/src)
 copy_aidl.commands = $(CHK_DIR_EXISTS) $$shell_path($$ANDROID_PACKAGE_SOURCE_DIR/aidl/com/android/vending/licensing) $(COPY_DIR) $$shell_path($$PWD/aidl) $$shell_path($$ANDROID_PACKAGE_SOURCE_DIR/aidl)
 PRE_TARGETDEPS += copy_src copy_aidl
 QMAKE_EXTRA_TARGETS += copy_src copy_aidl
